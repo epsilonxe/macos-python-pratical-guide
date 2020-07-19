78afe80db0b4c8feb0348d7f798a7af175fa529d
Ratthaprom Promkam <epsilonxe@VEKTOR.local>
2020-07-19 19:24:13 +0700

    altenate correct number

--- Lectures/17_Guessing_number.py
+++ Lectures/17_Guessing_number.py
@@ -7,7 +7,7 @@ while correct == 0:
 	x = int(input("Guess my number: "))
 	if x == my_number:
 		print(f"Yes, my number is {my_number}")
-		correct = 1
+		correct = 999
 	else:
 		print("No, it's not")
 		print("Try it again")
